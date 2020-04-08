9dc73b7881584a8777e18e98528887c14cae0912
Andy Ko <56237597+matthiasjk5@users.noreply.github.com>
2020-04-08 00:16:17 -0400

    Alert Context, State & Component

---
+++ client/src/context/alert/alertReducer.js
@@ -0,0 +1,13 @@
+import { SET_ALERT, REMOVE_ALERT } from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case SET_ALERT:
+      console.log(`alerReducer - state - ${state}`);
+      return [...state, action.payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== action.payload);
+    default:
+      return state;
+  }
+};
