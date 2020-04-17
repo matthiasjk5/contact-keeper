ad677cbb3fcfc832f8d2a4ccae2f5ef88cb6b2a6
Andy Ko <56237597+matthiasjk5@users.noreply.github.com>
2020-04-17 16:20:11 -0400

    Update Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -70,6 +70,33 @@ const ContactState = (props) => {
     }
   };
 
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
+
   // Clear Contact
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -83,11 +110,6 @@ const ContactState = (props) => {
   // Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // Filter Contacts
