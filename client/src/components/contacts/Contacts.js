8398342f5d0bca6859ac37ed95e209d7397890fe
Andy Ko <56237597+matthiasjk5@users.noreply.github.com>
2020-04-17 00:11:31 -0400

    Add Contact

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -16,14 +16,14 @@ const Contacts = () => {
     <Fragment>
       <TransitionGroup>
         {filtered !== null
-          ? filtered.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
                 <ContactItem contact={contact} />
               </CSSTransition>
             ))
-          : contacts.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
-                <ContactItem key={contact.id} contact={contact} />
+          : contacts.map((contact) => (
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
               </CSSTransition>
             ))}
       </TransitionGroup>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import { v4 } from 'uuid';
+import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -9,53 +9,44 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  CONTACT_ERROR,
 } from '../types';
 
-const ContactState = props => {
+const ContactState = (props) => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: 'Jill Johnson',
-        email: 'jill@yahoo.com',
-        phone: '111-111-1111',
-        type: 'personal'
-      },
-      {
-        id: 2,
-        name: 'Sara Watson',
-        email: 'sara@gmail.com',
-        phone: '222-222-2222',
-        type: 'personal'
-      },
-      {
-        id: 3,
-        name: 'Hary White',
-        email: 'harry@gmail.com',
-        phone: '333-333-3333',
-        type: 'professional'
-      }
-    ],
+    contacts: [],
     current: null,
-    filtered: null
+    filtered: null,
+    error: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
-  const addContact = contact => {
-    contact.id = v4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/contacts', contact, config);
+
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   // Delete Contact
-  const deleteContact = id => {
+  const deleteContact = (id) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
 
   // Set Current Contact
-  const setCurrent = contact => {
+  const setCurrent = (contact) => {
     dispatch({ type: SET_CURRENT, payload: contact });
   };
 
@@ -65,12 +56,12 @@ const ContactState = props => {
   };
 
   // Update Contact
-  const updateContact = contact => {
+  const updateContact = (contact) => {
     dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // Filter Contacts
-  const filterContacts = text => {
+  const filterContacts = (text) => {
     dispatch({ type: FILTER_CONTACTS, payload: text });
   };
 
@@ -85,13 +76,14 @@ const ContactState = props => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
       }}
     >
       {props.children}
